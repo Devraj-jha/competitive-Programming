a5f10337027d8dadcf87d1a20e98345d002610fe
Devraj-jha <devrajjha100806@gmail.com>
2025-05-28 19:45:28 +0530

    4th

---
+++ Daily grind show/Day1/deftypedefconst.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define owner 11
+int main(){
+
+
+cout<<owner;
+
+  
+  return 0;
+}
---
+++ Daily grind show/Day1/functions/passbyref.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+void printdata(const string &s){
+  cout<< s << "\n";
+}
+
+
+void update(int &a) {
+    a = a + 5;
+}
+//changes the original variable when we pass by refrecne.
+int main(){
+ios::sync_with_stdio(false); 
+cin.tie(NULL);              
+
+
+
+
+
+  return 0;
+}
