f252b4c73701dea11666b8160f1f65cd36172728
Devraj-jha <devrajjha100806@gmail.com>
2025-08-15 10:58:07 +0530

    Added constants overview with const, constexpr, mutable and example

---
+++ Till 1200 all topics required/Cpp syntax and speed tool./const.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+           
+ios::sync_with_stdio(false);
+cin.tie(NULL);
+
+const int x = 10;
+//makes a variable read only after initiziation;
+//works with; varaibels refreences pointers
+
+//use const & for large parameters to avoid copies
+
+//when 
+
+
+
+
+  return 0;
+}
+
