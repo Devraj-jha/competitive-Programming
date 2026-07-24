22424db052736e075f86d5bbce15e2330aecdacc
Devraj-jha <devrajjha100806@gmail.com>
2026-07-24 10:55:00 +0530

    imp: linkded list

--- Data_Structure/Linked_list/basics.cpp
+++ Data_Structure/Linked_list/basics.cpp
@@ -1,23 +1,23 @@
-//each box contains soe data.
-// each box also contaisn.. adress of the next.
-// 
+//each element -> store a data 
+// Where next element is ..
+
+
 #include <iostream>
 using namespace std; 
 
-struct ListNode {
-    int val; 
-    ListNode *next; 
-    
-    ListNode( int x ){
-        val = x; 
+struct  Node
+{
+    int data; 
+    Node* next; 
+    Node(int val){
+        data = val;
         next = nullptr;
-    };
-    // node contains pointers to next node and value..
-
-
+    }
 };
+
 int main(){
-    ListNode* head; 
-    ListNode* curr = head; 
-    
+   Node* head = new Node(10);
+   Node* second = new Node(20);
+    Node* third = new Node(30);
+   head->next = second;
 }
