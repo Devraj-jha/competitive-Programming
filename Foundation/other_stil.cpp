31a01b308cf9fa9c56322695b50fde221d79d10f
Devraj-jha <devrajjha100806@gmail.com>
2026-09-17 08:28:12 +0530

    other stl

---
+++ Foundation/other_stil.cpp
@@ -0,0 +1,23 @@
+// set stores unique element in sorted order.
+// unordered set -> stores unqiue element no order
+
+// map -> stores key value pair (sorted)
+// unordered map -> unsorted keyvalue pair.
+
+// stack -> lifo 
+
+// queue -> fifo
+
+// priority queue -> max heap gives largest element always
+
+// 
+// Need unique + sorted?          → set
+// Need unique + fast lookup?    → unordered_set
+
+// Need key → value + sorted?    → map
+// Need key → value + fast?      → unordered_map
+
+// LIFO?                          → stack
+// FIFO?                          → queue
+// Both ends?                     → deque
+// Highest/lowest priority first? → priority_queue
