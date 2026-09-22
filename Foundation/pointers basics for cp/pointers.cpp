487d0d006667060995e9d676aec5a249b832ce6a
Devraj-jha <devrajjha100806@gmail.com>
2026-09-22 09:08:04 +0530

    pointers

---
+++ Foundation/pointers basics for cp/pointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+    int x = 10; 
+    // variable x storing a value.
+    
+    // pointers stores value of another variable.
+
+    
+    int *p = & x; 
+    // here declaring p as a pointer. 
+
+    cout << *p << endl; 
+
+    // derefercing
+
+    // refrecne => another naame for the same variable.
+
+
+    int x = 10; 
+
+    int & r = x; 
+    // refernce alias. for the same term.
+
+
+}
+
